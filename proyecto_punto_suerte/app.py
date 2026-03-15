774a0b6adefb15d9d6e77e87ddfb3b058d62d395
karina <ke.rumipambag@uea.edu.ec>
2026-03-14 23:12:47 -0500

    CAMBIO GUARDADO

--- proyecto_punto_suerte/app.py
+++ proyecto_punto_suerte/app.py
@@ -1,23 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-# Página principal
-@app.route('/')
+@app.route("/")
 def inicio():
-    return "Bienvenido al Sistema Web Punto de la Suerte – Consulta de juegos de Lotería Nacional"
+    return "Bienvenido al sistema Punto de la Suerte - Lotería Nacional"
 
-# Ruta dinámica para usuarios
-@app.route('/usuario/<nombre>')
-def usuario(nombre):
-    return f'Bienvenido {nombre}, gracias por visitar el Punto de la Suerte.'
-
-# Ruta dinámica para productos
-@app.route('/producto/<juego>')
-def producto(juego):
-    return f'Juego disponible: {juego}. Puedes comprarlo en nuestro Punto de la Suerte.'
-
-import os
+@app.route("/producto/<nombre>")
+def producto(nombre):
+    return f"Producto disponible: {nombre}"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
