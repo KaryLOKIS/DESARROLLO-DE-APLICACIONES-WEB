cc421f44f117e84f1636133ec83c77fc05af0f23
karina <ke.rumipambag@uea.edu.ec>
2026-03-14 23:09:30 -0500

    CORRECCION PUERTO PARA RENDER

--- proyecto_punto_suerte/app.py
+++ proyecto_punto_suerte/app.py
@@ -17,5 +17,8 @@ def usuario(nombre):
 def producto(juego):
     return f'Juego disponible: {juego}. Puedes comprarlo en nuestro Punto de la Suerte.'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
