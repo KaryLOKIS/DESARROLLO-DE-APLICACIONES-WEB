02872c037a73b7d0690fe84c029d2bfa85c1a9b4
karina <ke.rumipambag@uea.edu.ec>
2026-03-14 23:50:08 -0500

    semana 10 archivos dinamicos

--- proyecto_punto_suerte/app.py
+++ proyecto_punto_suerte/app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
+# Página principal
 @app.route("/")
 def inicio():
-    return "Bienvenido al sistema Punto de la Suerte - Lotería Nacional"
+    return render_template("index.html")
 
+# Página Acerca de
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# Página Productos
+@app.route("/productos")
+def productos():
+    return render_template("productos.html")
+
+# Ruta dinámica ejemplo (producto individual)
 @app.route("/producto/<nombre>")
 def producto(nombre):
-    return f"Producto disponible: {nombre}"
+    return render_template("producto_individual.html", nombre=nombre)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
