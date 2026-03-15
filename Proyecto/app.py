5315eed03cbe6b8fe764c35b500927bade720ae6
karina <ke.rumipambag@uea.edu.ec>
2026-03-14 22:31:26 -0500

    SEMANA 13

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,31 +1,26 @@
 from flask import Flask, render_template
 
-print("ESTE ES MI APP CORRECTO")
-
 app = Flask(__name__)
 
-# Página principal
 @app.route("/")
 def inicio():
     return render_template("index.html")
 
-# Página acerca de
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
-# Página productos
 @app.route("/productos/")
 def productos():
-    lista_productos = ["Collar", "Comida Premium", "Ropa para perro", "Juguete"]
-    return render_template("productos.html", productos=lista_productos)
+    return render_template("productos.html")
 
-# Ruta dinámica
-@app.route("/producto/<nombre>/")
-def producto(nombre):
-    return f"Producto: {nombre} – disponible en Mundo Mascotas 🐾"
+@app.route("/clientes/")
+def clientes():
+    return render_template("clientes.html")
 
-print(app.url_map)   # ✅ AQUÍ AL FINAL
+@app.route("/factura/")
+def factura():
+    return render_template("factura.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ proyecto_punto_suerte/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Página principal
+@app.route('/')
+def inicio():
+    return "Bienvenido al Sistema Web Punto de la Suerte – Consulta de juegos de Lotería Nacional"
+
+# Ruta dinámica para usuarios
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Bienvenido {nombre}, gracias por visitar el Punto de la Suerte.'
+
+# Ruta dinámica para productos
+@app.route('/producto/<juego>')
+def producto(juego):
+    return f'Juego disponible: {juego}. Puedes comprarlo en nuestro Punto de la Suerte.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
